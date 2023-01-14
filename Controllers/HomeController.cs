4aafa97506c1e48385b4a4b8268b1340768da441
Rmassey11 <ronmassey11@gmail.com>
2023-01-14 10:52:15 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using LoanCalcMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using LoanCalcMVC.Helpers;
+namespace LoanCalcMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult App()
+        {
+            Loan loan = new();
+            loan.Payment = 0.0m;
+            loan.TotalInterest = 0.0m;
+            loan.TotalCost = 0.0m;
+            loan.Rate = 3.5m;
+            loan.Amount = 15000m;
+            loan.Term = 60;
+            return View(loan);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult App(Loan loan)
+        {
+            var loanHelper = new LoanHelper();
+            Loan newloan = loanHelper.GetPayments(loan);
+            return View(newloan);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
